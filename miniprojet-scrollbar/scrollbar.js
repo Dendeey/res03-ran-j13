61da5325fe39b902c2111359b566fc321e851d9f
Dendeey <davidsim1035@gmail.com>
2022-11-24 14:01:19 +0000

    Mini projet scrollbar

--- miniprojet-scrollbar/scrollbar.js
+++ miniprojet-scrollbar/scrollbar.js
@@ -1,15 +1,47 @@
-function getNextSection(section1){
-    if(section1.id === "red"){
-        return document.getElementById("orange");
-    }else{
-        return section1;
+
+let section1 = document.getElementById("red");
+let section2 = document.getElementById("orange");
+let section3 = document.getElementById("yellow");
+let section4 = document.getElementById("green");
+let section5 = document.getElementById("blue");
+let section6 = document.getElementById("purple");
+
+let position1 = section1.getBoundingClientRect().top;
+console.log(position1);
+let position2 = section2.getBoundingClientRect().top;
+let position3 = section3.getBoundingClientRect().top;
+let position4 = section4.getBoundingClientRect().top;
+let position5 = section5.getBoundingClientRect().top;
+let position6 = section6.getBoundingClientRect().top;
+let sections = document.querySelectorAll("body > main >section");
+/*
+function nextPos(current){
+    for(let i = 0; i < ){
+        if(current.deltaY < 0){
+            
+        }
     }
-    
-    if(section2.id === "orange"){
-        
-    }else{
+}
+
+function prevPos(current){
+    if(current.deltaY < 0){
         
     }
 }
 
+for(let i = 0; i < sections.length; i++){
+    sections[i].addEventListener("wheel", function(){
+        if(event.deltaY > 0) // je vais vers le bas
+        {
+            scroll(position1, position2);
+            console.log("Scrolled!");
+        }
+        else if(event.deltaY < 0) // je vais vers le haut
+        {
+            scroll(position2, position1);
+            console.log("Scrolled!");
+        }
+    });
+}
+*/
 
