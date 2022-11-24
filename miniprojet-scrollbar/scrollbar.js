6d26184cbfe5a328b8af7fefe002c8cd57eea375
Dendeey <davidsim1035@gmail.com>
2022-11-24 11:32:33 +0000

    Mini projet scrollbar

---
+++ miniprojet-scrollbar/scrollbar.js
@@ -0,0 +1,15 @@
+function getNextSection(section1){
+    if(section1.id === "red"){
+        return document.getElementById("orange");
+    }else{
+        return section1;
+    }
+    
+    if(section2.id === "orange"){
+        
+    }else{
+        
+    }
+}
+
+
