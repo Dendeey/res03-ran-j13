a4f6519658b8066ddb3619c7b308c30ffa494afb
Dendeey <davidsim1035@gmail.com>
2022-11-24 15:28:59 +0000

    Mini projet scrollbar

--- miniprojet-scrollbar/scrollbar.js
+++ miniprojet-scrollbar/scrollbar.js
@@ -9,39 +9,79 @@ let section6 = document.getElementById("purple");
 let position1 = section1.getBoundingClientRect().top;
 console.log(position1);
 let position2 = section2.getBoundingClientRect().top;
+console.log(position2);
 let position3 = section3.getBoundingClientRect().top;
+console.log(position3);
 let position4 = section4.getBoundingClientRect().top;
+console.log(position4);
 let position5 = section5.getBoundingClientRect().top;
+console.log(position5);
 let position6 = section6.getBoundingClientRect().top;
+console.log(position6);
 let sections = document.querySelectorAll("body > main >section");
-/*
-function nextPos(current){
-    for(let i = 0; i < ){
-        if(current.deltaY < 0){
-            
-        }
+
+
+function GetNextPos(current){
+    if(current.id==="red"){
+        return document.getElementById("orange");
+    }
+    if(current.id==="orange"){
+        return document.getElementById("yellow");
+    }
+    if(current.id==="yellow"){
+        return document.getElementById("green");
+    }
+    if(current.id==="green"){
+        return document.getElementById("blue");
+    }
+    if(current.id==="blue"){
+        return document.getElementById("purple");
+    }
+    if(current.id==="purple"){
+        return undefined;
     }
 }
 
-function prevPos(current){
-    if(current.deltaY < 0){
-        
+function GetPrevPos(current){
+    if(current.id==="purple"){
+        return document.getElementById("blue");
+    }
+    if(current.id==="blue"){
+        return document.getElementById("green");
+    }
+    if(current.id==="green"){
+        return document.getElementById("yellow");
+    }
+    if(current.id==="yellow"){
+        return document.getElementById("orange");
+    }
+    if(current.id==="orange"){
+        return document.getElementById("red");
+    }
+    if(current.id==="red"){
+        return undefined;
     }
 }
+
 
 for(let i = 0; i < sections.length; i++){
-    sections[i].addEventListener("wheel", function(){
-        if(event.deltaY > 0) // je vais vers le bas
+    sections[i].addEventListener("wheel", function(event){
+        
+        let currentScrollPosition = document.documentElement.scrollTop; 
+        
+        if(event.deltaY > 0 && GetNextPos(sections[i]) != undefined) // je vais vers le bas
         {
-            scroll(position1, position2);
-            console.log("Scrolled!");
+            let NextPos = GetNextPos(sections[i]).getBoundingClientRect().top;
+            scroll(0, NextPos + currentScrollPosition);
+            console.log(scroll);
         }
-        else if(event.deltaY < 0) // je vais vers le haut
+        else if(event.deltaY < 0 && GetPrevPos(sections[i] != undefined)) // je vais vers le haut
         {
-            scroll(position2, position1);
-            console.log("Scrolled!");
+            let PrevPos = GetPrevPos(sections[i]).getBoundingClientRect().top;
+            scroll(0, PrevPos + currentScrollPosition);
+            console.log(scroll);
         }
     });
 }
-*/
+
 
